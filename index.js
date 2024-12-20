e8cd6ca5705254db1c781e31d7929582e6f8b5c6
Bruno Desktop <carecsnay1@gmail.com>
2024-12-20 20:28:36 -0300

    Feat: Added function to AC button.

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 const dqs = (element) => document.querySelector(element);
 const dqsa = (element) => document.querySelectorAll(element);
 
-const numbersButtons = dqsa(["data-number"]);
-const operationButtons = dqsa(["data-operator"]);
+const numbersButtons = dqsa("[data-number]");
+const operationButtons = dqsa("[data-operator]");
 
-const equalsButton = dqs(["data-equals"]);
-const deleteButton = dqs(["data-delete"]);
-const allClearButton = dqs(["data-all-clear"]);
-const previousButton = dqs(["data-previous-operand"]);
-const currentButton = dqs(["data-current-operand"]);
+const equalsButton = dqs("[data-equals]");
+const deleteButton = dqs("[data-delete]");
+const allClearButton = dqs("[data-all-clear]");
+const previousOP = dqs("[data-previous-operand]");
+const currentOP = dqs("[data-current-operand]");
+
+class Calculator {
+    constructor(previousOP, currentOP) {
+        this.previousOP = previousOP;
+        this.currentOP = currentOP;
+    }
+    clear() {
+        this.previousOperand = "";
+        this.currentOperand = "";
+        this.operation = undefined;
+    }
+
+    updateDisplay() {
+        this.previousOP.innerText = this.previousOperand;
+        this.currentOP.innerText = this.currentOperand;
+    }
+}
+
+const calculator = new Calculator(previousOP, currentOP);
+
+allClearButton.addEventListener("click", () => {
+    calculator.clear();
+    calculator.updateDisplay();
+});
