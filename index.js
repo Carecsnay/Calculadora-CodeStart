cbc3c584184e97aedf437c22a86580cd5d30a4e7
Bruno Benicio - Escritório <carecsnay1@gmail.com>
2024-12-20 15:30:28 -0300

    Chore: More javascript attributes and elements select.

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const dqs = (element) => document.querySelector(element);
 const dqsa = (element) => document.querySelectorAll(element);
 
+const numbersButtons = dqsa(["data-number"]);
+const operationButtons = dqsa(["data-operator"]);
+
+const equalsButton = dqs(["data-equals"]);
+const deleteButton = dqs(["data-delete"]);
+const allClearButton = dqs(["data-all-clear"]);
+const previousButton = dqs(["data-previous-operand"]);
+const currentButton = dqs(["data-current-operand"]);
